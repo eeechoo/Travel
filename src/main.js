8f16adaeaa224f4aed1f754ce64fa60dd9693453
eeechoo <caiminghui369@gmail.com>
2018-04-26 11:49:10 +0800

    project init
    删除一些不必要的东西，增加一些适配性文件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'// 解决移动端点击事件300ms延迟问题
+import './assets/styles/reset.css'// 解决不同手机型号默认渲染效果不同问题
+import './assets/styles/border.css'// 解决多倍屏 1像素边框问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
