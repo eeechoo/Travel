1432b8a0e27a9456f0fafd4d176d7a14e6bbb654
eeechoo <caiminghui369@gmail.com>
2018-04-26 14:55:55 +0800

    add header
    添加home page上的header组件

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'// 解决移动端点击事件300ms延迟问题
-import './assets/styles/reset.css'// 解决不同手机型号默认渲染效果不同问题
-import './assets/styles/border.css'// 解决多倍屏 1像素边框问题
+import 'styles/reset.css'// 解决不同手机型号默认渲染效果不同问题
+import 'styles/border.css'// 解决多倍屏 1像素边框问题
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
